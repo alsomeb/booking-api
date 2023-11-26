6238897be27c86c08b26b3e322b5f2369007d5ce
alsomeb <alsomeb@gmail.com>
2023-11-26 20:26:34 +0100

    structs.go comments about pointers

--- structs/structs.go
+++ structs/structs.go
@@ -20,6 +20,18 @@ type Booking struct {
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
 
+// MongoClient - holds a pointer to the MongoDB Client
+// When you create an instance of this struct and pass it around, you are essentially passing a reference to the underlying mongo.Client instance
 type MongoClient struct {
 	MongoClient *mongo.Client
 }
+
+/*
+	In Go, when you pass a struct to a function, a copy of the struct is made unless you explicitly pass it as a POINTER.
+	When you pass a struct that contains a pointer to another object (in this case, a MongoDB client),
+	you are still passing a copy of the struct, but since the pointer inside the struct points to the same underlying object, modifications to the object are visible to all references.
+
+	When you pass *structs.MongoClient to functions like addBooking, getAllBookings, and getBookingById, you are passing the actual client and not the address of the client (&variable).
+	The * in this context is used to dereference the pointer inside the MongoClient struct, giving you the actual mongo.Client object.
+
+*/
